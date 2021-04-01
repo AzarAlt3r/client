26350c7702915a28c1efeb47b3f45b831510f436
AzarAlt3r <jonathanalternate@hotmail.com>
2021-04-01 15:55:39 -0600

    Actualizacion de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {routing,appRoutingProviders} from './app.routing'
 import { AppComponent } from './app.component';
+
+import { HomeComponent } from './components/home.component';
+import {UserEditComponent} from './components/user-edit.component';
+import { ArtistListComponent } from './components/artist-list.component';
+import { ArtistAddComponent } from './components/artist-add.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserEditComponent,
+    ArtistListComponent,
+    HomeComponent,
+    ArtistAddComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,20 @@
+import { ModuleWithProviders } from '@angular/core'; 
+import { Routes, RouterModule } from '@angular/router'; 
+// import user 
+import { UserEditComponent } from './components/user-edit.component'; 
+import { HomeComponent } from './components/home.component'
+// import artist
+import { ArtistListComponent } from './components/artist-list.component';
+import { ArtistAddComponent } from './components/artist-add.component';
+//import home
+
+
+const appRoutes: Routes = [ 
+    {path: '', component: HomeComponent}, 
+    {path: 'artistas/:page', component: ArtistListComponent},
+    {path: 'crear-artista', component: ArtistAddComponent},
+    {path: 'mis-datos', component: UserEditComponent}, 
+    {path: '**', component: HomeComponent}
+]; 
+export const appRoutingProviders: any [] =[]; 
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
